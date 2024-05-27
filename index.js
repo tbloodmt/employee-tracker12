4142f6df08fa0666f27b1c2552f260b58bc3faa7
Matthew <trueblood.mt@gmail.com>
2024-05-27 13:11:05 -0500

    adding different functions

--- index.js
+++ index.js
@@ -87,6 +87,70 @@ function viewAllEmployees(){
     })
 }
 
+function addADepartment(){
+    inquirer.prompt([
+        {
+            type:"input",
+            name:"department",
+            messagae:"what department would you like to add? "
+        }
+    ]).then((departmentAnswer)=>{
+        let departmentName = departmentAnswer.department
+
+        db.query("INSERT INTO department (dept_name) VALUES ($1)",[departmentName],function(err,res){
+            err?console.error(err):viewAllDepartments();
+        })
+    })
+}
+
+function addARole(){
+    db.query("SELECT * FROM department",function(err,res){
+        if (err){
+            console.error(err);
+            start()
+        } 
+
+        const departmentList = res.rows.map((department)=>({
+            value:department.id,
+            name:department.dept_name
+        }))
+        inquirer.prompt([
+            {
+                type:"input",
+                name:"newRole",
+                message:"What role would you like to add? "
+            },
+            {
+                type:"input",
+                name:"salary",
+                message:"How much do they make? "
+            },
+            {
+                type:"list",
+                name:"roleList",
+                message:"What department are you adding the role to? ",
+                choices:departmentList
+            }
+        ]).then((roleAnswers)=>{
+            console.log(parseInt(roleAnswers.salary))
+            let roleTitle = roleAnswers.newRole
+            let roleSalary = parseInt(roleAnswers.salary)
+            let department = roleAnswers.roleList
+
+
+            db.query("INSERT INTO role (title,salary,department_id) VALUES ($1,$2,$3)",[roleTitle,roleSalary,department],function(err,res){
+                console.log(res)
+                err?console.error(err):viewAllRoles();
+            })
+        })
+    })
+
+}
+
+function addAEmployee(){
+    db.query("SELECT * FROM employee,")
+}
+
 function quit(){
     console.log("bye")
     process.exit()
